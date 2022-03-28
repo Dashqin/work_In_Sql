7189999b1d7d88b073430ce97e0ca23eebef05a9
Dashqin <7btpdfk@code.edu.az>
2022-03-28 23:52:04 +0300

    hometaskfor03.29.2022

---
+++ 03.28.2022/03.28.2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._28._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singer cd = new Singer("Ahmed", "Kerimov", 56);
+        }
+    }
+}
