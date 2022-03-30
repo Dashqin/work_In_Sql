6b678eeb6603d00d841bad6e26331f7dba02e835
Dashqin <7btpdfk@code.edu.az>
2022-03-31 02:41:02 +0300

    03.31.2022

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Square sq = new Square(5);
+            Console.WriteLine();
+        //    RectAngular ra = new RectAngular(3,5);
+        //    Console.WriteLine(ra.CalcArea);
+        }
+      
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Square.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    class Square : Figure
+    {
+        private int _Side;
+        public Square(int side)
+        {
+            _Side = side;
+        }
+        public int Side 
+        {
+            get
+            {
+                return _Side;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _Side = value;
+                }
+            }
+        }
+        public override void CalcArea()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("square ishleyir");
+        }
+    }
+}
